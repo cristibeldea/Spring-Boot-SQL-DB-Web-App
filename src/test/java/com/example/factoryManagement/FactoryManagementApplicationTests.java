8b1a5873da05380a285409d5039eb748f8a12407
cristibeldea <cristibeldea@gmail.com>
2025-09-13 21:28:30 +0300

    some small changes

--- src/test/java/com/example/factoryManagement/FactoryManagementApplicationTests.java
+++ src/test/java/com/example/factoryManagement/FactoryManagementApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.loginAndWelcome;
+package com.example.factoryManagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoginAndWelcomeApplicationTests {
+class FactoryManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
